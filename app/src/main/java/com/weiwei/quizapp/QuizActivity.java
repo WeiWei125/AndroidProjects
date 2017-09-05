06691a7fa744f6835ebf86907e7ea34cf40f06f1
Wei Zhao <wezhao25@umd.edu>
2017-09-04 23:36:44 -0700

    Create main view and add onClickListeners to buttons

--- app/src/main/java/com/weiwei/quizapp/QuizActivity.java
+++ app/src/main/java/com/weiwei/quizapp/QuizActivity.java
@@ -2,12 +2,36 @@ package com.weiwei.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+
+        final TextView answerResponseView= (TextView) findViewById(R.id.answer_response_text);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answerResponseView.setText(getResources().getString(R.string.right_answer_response));
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answerResponseView.setText(getResources().getString(R.string.wrong_answer_response));
+            }
+        });
     }
 }
